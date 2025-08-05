e144caf038124a3162933b8a312861d0645dc22a
zahraaddina <addinazahra14@gmail.com>
2025-08-05 23:36:36 +0700

    js for materi, biar kode rapi

---
+++ Materi/materi.js
@@ -0,0 +1,8 @@
+//popup
+function openForm() {
+  document.getElementById("formOverlay").style.display = "flex";
+}
+
+function closeForm() {
+  document.getElementById("formOverlay").style.display = "none";
+}
